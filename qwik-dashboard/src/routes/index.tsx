12edea786d7748824e5be346ae2fb3ac4089e2df
G9 Aerospace <g9militants@gmail.com>
2024-04-11 21:41:33 +0530

    better

--- qwik-dashboard/src/routes/index.tsx
+++ qwik-dashboard/src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { component$ } from "@builder.io/qwik";
 import { Link, type DocumentHead } from "@builder.io/qwik-city";
-// import { HiArrowRightOutline } from "@qwikest/icons/heroicons";
+import { HiArrowRightOutline } from "@qwikest/icons/heroicons";
 import styles from "./home.module.css";
-<div class={styles.shape} />;
+
 export default component$(() => {
   return (
     <main class="flex min-h-screen flex-col p-6">
@@ -11,7 +11,7 @@ export default component$(() => {
       </div>
       <div class="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div class="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div class="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-blue-500 border-l-transparent border-r-transparent" />
+          <div class={styles.shape} />
           <p class="text-xl text-gray-800 md:text-3xl md:leading-normal">
             <strong>Welcome to LRDQwik.</strong> This is the example for the{" "}
             <Link
@@ -36,7 +36,7 @@ export default component$(() => {
             class="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span>
-            {/* <HiArrowRightOutline /> */}
+            <HiArrowRightOutline />
           </Link>
         </div>
         <div class="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
